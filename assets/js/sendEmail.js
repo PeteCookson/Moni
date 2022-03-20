86080cbe552222610d228cb4c94090f6be910bf0
PeteCookson <pgcookson@gmail.com>
2022-03-20 14:27:25 +0000

    Adjusted padding on toggle for small screen size, added alert for email response

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,6 +7,7 @@ function sendMail(contactForm) {
         .then(
             function(response) {
                 console.log("SUCCESS", response);
+                alert("Message Sent!");
             },
             function(error) {
                 console.log("FAILED", error);
